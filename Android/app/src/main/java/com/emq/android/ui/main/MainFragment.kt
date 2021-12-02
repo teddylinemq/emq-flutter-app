f2fcbdaf310da67a9384ae1daed1571793e4db6b
TeddyLinEmq <teddylin@emq.com>
2021-12-02 10:43:17 +0800

    Add flutter to android app

---
+++ Android/app/src/main/java/com/emq/android/ui/main/MainFragment.kt
@@ -0,0 +1,37 @@
+package com.emq.android.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.emq.android.R
+import io.flutter.embedding.android.FlutterActivity
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.main_fragment, container, false)
+        view.findViewById<Button>(R.id.welcome_flutter).setOnClickListener {
+            startActivity(FlutterActivity.createDefaultIntent(requireContext()))
+        }
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+}
